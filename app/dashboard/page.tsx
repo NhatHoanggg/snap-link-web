1beaea92d10354582d6a230370f174c4dea741de
Nhat Hoang <103094181+NhatHoanggg@users.noreply.github.com>
2025-04-06 03:25:26 +0700

    update/ react day picker 4

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -38,7 +38,7 @@ export default function DashboardPage() {
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
           <div className="flex items-center space-x-2">
-            <CalendarDateRangePicker date={dateRange} setDate={setDateRange} />
+            <CalendarDateRangePicker date={dateRange} setDate={(date) => date && setDateRange(date)} />
             <Button>
               <Download className="mr-2 h-4 w-4" />
               Download Report
