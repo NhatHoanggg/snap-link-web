3dc99d8efbabc5cf85e4b4a4cb50ee3278efe353
Nhat Hoang <103094181+NhatHoanggg@users.noreply.github.com>
2025-04-06 03:15:46 +0700

    update/ react day picker 3

--- components/dashboard/date-range-picker.tsx
+++ components/dashboard/date-range-picker.tsx
@@ -8,20 +8,21 @@ import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 
-interface CalendarDateRangePickerProps {
-  date: DateRange
-  setDate: (date: DateRange) => void
+interface DateRangePickerProps {
+  date: DateRange | undefined
+  setDate: (date: DateRange | undefined) => void
   className?: string
+  align?: "center" | "start" | "end"
 }
 
-export function CalendarDateRangePicker({ date, setDate, className }: CalendarDateRangePickerProps) {
+export function CalendarDateRangePicker({ date, setDate, className, align = "end" }: DateRangePickerProps) {
   return (
     <div className={cn("grid gap-2", className)}>
       <Popover>
         <PopoverTrigger asChild>
           <Button
             id="date"
-            variant={"outline"}
+            variant="outline"
             className={cn("w-[260px] justify-start text-left font-normal", !date && "text-muted-foreground")}
           >
             <CalendarIcon className="mr-2 h-4 w-4" />
@@ -34,15 +35,14 @@ export function CalendarDateRangePicker({ date, setDate, className }: CalendarDa
                 format(date.from, "LLL dd, y")
               )
             ) : (
-              <span>Pick a date</span>
+              <span>Pick a date range</span>
             )}
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="end">
+        <PopoverContent className="w-auto p-0" align={align}>
           <Calendar
             initialFocus
             mode="range"
-            required
             defaultMonth={date?.from}
             selected={date}
             onSelect={setDate}
